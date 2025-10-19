8136c0ec6faa7d14d1070b6a9ec1eea82ecd6ecb
Amante Reale <criticalsession@protonmail.ch>
2025-10-19 08:16:39 +0200

    init

---
+++ CloverApi/CloverApi.Api/CloverApiClient.cs
@@ -0,0 +1,27 @@
+﻿namespace CloverApi.Api;
+
+public class CloverApiClient
+{
+    private readonly HttpClient _httpClient;
+    private readonly bool _disposeHttpClient;
+
+    public CloverApiClient(HttpClient httpClient)
+    {
+        _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+        _disposeHttpClient = false;
+    }
+
+    public CloverApiClient()
+    {
+        _httpClient = new HttpClient();
+        _disposeHttpClient = true;
+    }
+
+    public void Dispose()
+    {
+        if (_disposeHttpClient)
+        {
+            _httpClient.Dispose();
+        }
+    }
+}
