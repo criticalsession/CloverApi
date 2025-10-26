5ba02a0f7bc6c4b4cb0984bfb330c8e7db1e8dc6
Amante Reale <criticalsession@protonmail.ch>
2025-10-26 07:55:08 +0100

    initial thread summary and page model

---
+++ CloverApi/CloverApi.Api/Models/ThreadPage.cs
@@ -0,0 +1,17 @@
+﻿namespace CloverApi.Api.Models;
+
+/// <summary>
+/// Comprehensive list of all threads grouped by page number
+/// </summary>
+public class ThreadPage
+{
+    /// <summary>
+    /// The page number that the threads are on
+    /// </summary>
+    public int Page { get; set; }
+
+    /// <summary>
+    /// The array of thread summary objects
+    /// </summary>
+    public ThreadSummary[] Threads { get; set; }
+}
---
+++ CloverApi/CloverApi.Api/Models/ThreadSummary.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CloverApi.Api.Models;
+
+public class ThreadSummary
+{
+    /// <summary>
+    /// The OP ID of a thread
+    /// </summary>
+    [JsonPropertyName("no")]
+    public int ThreadId { get; set; }
+
+    /// <summary>
+    /// The UNIX timestamp marking the last time the thread was modified (post added/modified/deleted, thread closed/sticky settings modified)
+    /// </summary>
+    [JsonPropertyName("last_modified")]
+    public int LastModifiedUnixTimestamp { get; set; }
+
+    /// <summary>
+    /// The UTC DateTime marking the last time the thread was modified
+    /// </summary>
+    public DateTime LastModifiedUTC => DateTimeOffset.FromUnixTimeSeconds(LastModifiedUnixTimestamp).UtcDateTime;
+
+    /// <summary>
+    /// A numeric count of the number of replies in the thread
+    /// </summary>
+    public int Replies { get; set; }
+}
