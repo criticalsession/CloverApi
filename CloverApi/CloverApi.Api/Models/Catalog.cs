e2599327757862561f8c37b679d78eec44735320
Amante Reale <criticalsession@protonmail.ch>
2025-10-26 07:59:45 +0100

    initial catalog and catalog-thread models

---
+++ CloverApi/CloverApi.Api/Models/Catalog.cs
@@ -0,0 +1,13 @@
+﻿namespace CloverApi.Api.Models;
+
+/// <summary>Comprehensive list of all threads+attributes on a board. Every thread is grouped by page.</summary>
+public class Catalog
+{
+    /// <summary>Page number.</summary>
+    public int Page { get; set; }
+    
+    /// <summary>
+    /// List of threads on the page.
+    /// </summary>
+    public CatalogThread[] Threads { get; set; }
+}
---
+++ CloverApi/CloverApi.Api/Models/CatalogThread.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CloverApi.Api.Models;
+
+public class CatalogThread : ThreadPost
+{
+    /// <summary>Number of replies minus the number of previewed replies</summary>
+    public int OmittedPosts { get; set; }
+
+    /// <summary>Number of image replies minus the number of previewed image replies</summary>
+    public int OmittedImages { get; set; }
+
+    /// <summary>
+    /// The UNIX timestamp marking the last time the thread was modified (post added/modified/deleted,
+    /// thread closed/sticky settings modified)
+    /// </summary>
+    [JsonPropertyName("last_modified")]
+    public int LastModifiedUnixTimestamp { get; set; }
+
+    public DateTime LastModifiedUTC => DateTimeOffset.FromUnixTimeSeconds(LastModifiedUnixTimestamp).UtcDateTime;
+
+    /// <summary>JSON representation of the most recent replies to a thread</summary>
+    public ThreadPost[] LastReplies { get; set; }
+}
